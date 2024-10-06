5131664a26bb4c19a18fd8458f54d2bc3dad6220
Akash4056 <akashsaha1313@gmail.com>
2024-10-06 16:56:59 +0530

    navbar updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8 -rotate-90">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                     </svg>
-                    <span className='font-bold text-xl'>airbnc</span>
+                    <span className='font-bold text-xl'>StayScape</span>
                 </Link>
                 {/* Logo end */}
 
@@ -51,7 +51,7 @@ const Header = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8 -rotate-90">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                     </svg>
-                    <span className='font-bold text-xl'>airbnc</span>
+                    <span className='font-bold text-xl'>StayScape</span>
                 </Link>
             </div>
             {/* Logo end */}
